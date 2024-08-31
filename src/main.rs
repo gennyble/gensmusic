9c4ad9cf41ac7e9b95fa2760203b71083465d13f
gennyble <gen@nyble.dev>
2024-08-31 06:13:18 -0500

    Some kind of mvp maybe
    Can't pause lmfao

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,224 @@
 use std::{
+	ffi::OsStr,
 	fs::File,
-	sync::mpsc::{sync_channel, RecvTimeoutError, SyncSender},
+	path::PathBuf,
+	sync::{
+		atomic::{AtomicBool, Ordering},
+		mpsc::{sync_channel, Receiver, SyncSender, TryRecvError},
+		Arc,
+	},
+	thread::JoinHandle,
 	time::Duration,
 };
 
-use raplay::{
-	source::{SineSource, Symph},
-	Sink,
-};
+use eframe::{egui, NativeOptions};
+use egui_extras::{Column, TableBuilder};
+use raplay::{source::Symph, CallbackInfo, Sink, Timestamp};
 
 fn main() {
-	let (tx, rx) = sync_channel::<Msg>(8); //gen- idk why 8
+	let nopt = NativeOptions::default();
+	eframe::run_native(
+		"egui app",
+		nopt,
+		Box::new(|cc| Ok(Box::new(GensMusic::new(cc)))),
+	)
+	.unwrap();
+}
 
-	let mut sink = Sink::default();
-	sink.volume(0.8).unwrap();
-	sink.on_callback(Some(move |_cb_info| over(&tx))).unwrap();
-	sink.on_err_callback(Some(cb_error)).unwrap();
+pub enum GenMsg {
+	MediaEnded,
+	TimestampWakeup,
+}
 
-	println!("volume = {:?}", sink.get_volume());
+struct GensMusic {
+	files: Vec<PathBuf>,
 
-	let file = File::open("test.mp3").unwrap();
-	let src = Symph::try_new(file, &Default::default()).unwrap();
-	sink.load(src, true).unwrap();
-	sink.play(true).unwrap();
+	/// Communication from other threads
+	tx: SyncSender<GenMsg>,
+	rx: Receiver<GenMsg>,
 
-	let info = sink.get_info();
-	println!("{}hz // {}", info.sample_rate, info.sample_format);
+	// === Graphical ==
+	ctx: egui::Context,
 
-	loop {
-		match rx.recv_timeout(Duration::from_secs(1)) {
-			Ok(msg) => {
-				println!("Finished!");
-				break;
+	/// Sound output and control via raplay
+	sink: Sink,
+	next: Option<PathBuf>,
+	timekeeper: Option<JoinHandle<()>>,
+	timekeeper_cancel: Arc<AtomicBool>,
+}
+
+impl GensMusic {
+	pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+		// Gather all MP3 files in the current diretory
+		let mut mp3_files = vec![];
+		for entry in std::fs::read_dir(".").unwrap() {
+			let entry = entry.unwrap();
+
+			match entry.path().extension() {
+				Some(ext) if ext == OsStr::new("mp3") => {
+					mp3_files.push(entry.path());
+				}
+				_ => (),
 			}
-			Err(RecvTimeoutError::Disconnected) => {
-				eprintln!("Error: channel disconnected");
-				break;
+		}
+
+		// Channel for communicating back to the GUI thread
+		let (tx, rx) = sync_channel::<GenMsg>(8); //gen- idk why 8
+
+		// Make me a sink, baby
+		let sink = Sink::default();
+
+		let cb_tx = tx.clone();
+		let cb_ctx = cc.egui_ctx.clone();
+		sink.on_callback(Some(move |cbi| sink_cb(cbi, &cb_tx, &cb_ctx)))
+			.unwrap();
+
+		let cb_err_tx = tx.clone();
+		sink.on_err_callback(Some(move |err| sink_cb_err(err, &cb_err_tx)))
+			.unwrap();
+
+		Self {
+			files: mp3_files,
+			tx,
+			rx,
+			ctx: cc.egui_ctx.clone(),
+			sink,
+			next: None,
+			timekeeper: None,
+			timekeeper_cancel: Arc::new(AtomicBool::new(false)),
+		}
+	}
+
+	fn is_playing(&self) -> bool {
+		//SAFTEY: if we lost the mutex it's so joever
+		self.sink.is_playing().unwrap()
+	}
+
+	fn handle_events(&mut self) {
+		loop {
+			match self.rx.try_recv() {
+				Ok(GenMsg::MediaEnded) => todo!(),
+				Ok(GenMsg::TimestampWakeup) => (),
+				Err(TryRecvError::Disconnected) => panic!(),
+				Err(TryRecvError::Empty) => break,
 			}
-			Err(RecvTimeoutError::Timeout) => {
-				println!("playing: {}", sink.is_playing().unwrap());
-				continue;
-				match sink.get_timestamp() {
-					Ok(ts) => {
-						println!(
-							"{:.2} / {:.2}",
-							ts.current.as_secs_f32(),
-							ts.total.as_secs_f32()
-						);
-					}
-					Err(e) => {
-						eprintln!("Failed to get timestamp: {e}");
-						break;
-					}
-				};
-			}
+		}
+	}
+
+	fn start_timekeeper(&mut self) {
+		if self.timekeeper.is_none() {
+			let cancel = self.timekeeper_cancel.clone();
+			let tx = self.tx.clone();
+			let ctx = self.ctx.clone();
+
+			let hwnd = std::thread::spawn(move || loop {
+				std::thread::sleep(Duration::from_millis(250));
+
+				if cancel.load(Ordering::Relaxed) {
+					break;
+				}
+
+				tx.send(GenMsg::TimestampWakeup).unwrap();
+				ctx.request_repaint();
+			});
+
+			self.timekeeper = Some(hwnd);
+		}
+	}
+
+	fn stop_timekeeper(&mut self) {
+		self.timekeeper_cancel.store(true, Ordering::Release);
+		// we drop it here and let it wakeup and die later
+		let _ = self.timekeeper.take();
+	}
+
+	fn change_media<P: Into<PathBuf>>(&mut self, path: P) {
+		if self.is_playing() {
+			self.sink.pause().unwrap();
+		}
+
+		self.next = Some(path.into());
+	}
+
+	fn start_playing(&mut self) {
+		if let Some(path) = self.next.as_deref() {
+			let file = File::open(path).unwrap();
+			let symph = Symph::try_new(file, &Default::default()).unwrap();
+
+			self.sink.load(symph, true).unwrap();
+			self.start_timekeeper();
 		}
 	}
 }
 
-enum Msg {
-	Over,
-}
+impl eframe::App for GensMusic {
+	fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+		self.handle_events();
 
-fn over(tx: &SyncSender<Msg>) {
-	if let Err(e) = tx.send(Msg::Over) {
-		eprintln!("Failed to send over: {e}");
+		egui::TopBottomPanel::bottom("controls")
+			.frame(egui::Frame::default().inner_margin(8.0))
+			.show(ctx, |ui| {
+				if self.is_playing() {
+					if ui.button("Pause").clicked() {}
+				} else {
+					if ui.button("Play").clicked() {
+						self.start_playing();
+					}
+				}
+
+				if let Some(path) = self.next.as_deref() {
+					ui.horizontal(|ui| {
+						if self.is_playing() {
+							let timestamp = self.sink.get_timestamp().unwrap();
+							ui.label(format!(
+								"{}/{}",
+								timestamp.current.as_secs(),
+								timestamp.total.as_secs()
+							));
+						} else {
+							ui.label("0 / 0");
+						}
+
+						ui.separator();
+
+						ui.label(path.to_string_lossy());
+					});
+				}
+			});
+
+		egui::CentralPanel::default().show(ctx, |ui| {
+			TableBuilder::new(ui)
+				.striped(true)
+				.column(Column::remainder())
+				.header(20.0, |mut header| {
+					header.col(|ui| {
+						ui.heading("Path");
+					});
+				})
+				.body(|mut body| {
+					// So clearly a clone here is unoptimal, but.
+					for file in self.files.clone() {
+						body.row(20.0, |mut row| {
+							row.col(|ui| {
+								if ui.label(file.to_string_lossy()).clicked() {
+									self.change_media(file);
+								}
+							});
+						});
+					}
+				});
+		});
 	}
 }
 
-fn cb_error(e: raplay::Error) {
-	eprintln!("playback error: {e}");
+fn sink_cb(cbi: CallbackInfo, tx: &SyncSender<GenMsg>, ctx: &egui::Context) {
+	match cbi {
+		CallbackInfo::SourceEnded => tx.send(GenMsg::MediaEnded).unwrap(),
+		CallbackInfo::PauseEnds(_) => todo!(),
+		_ => todo!(),
+	}
+
+	ctx.request_repaint();
 }
+
+fn sink_cb_err(_err: raplay::Error, tx: &SyncSender<GenMsg>) {}
